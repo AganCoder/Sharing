1a77b450c09b3c9c25817a097a02ba8192e656ff
Noah <noah.gao@zoom.us>
2020-03-29 10:41:47 +0800

    局域网 UDP 通信初步成功

---
+++ Sharing/Pods/Target Support Files/Pods-UploadSetupUI/Pods-UploadSetupUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UploadSetupUI : NSObject
+@end
+@implementation PodsDummy_Pods_UploadSetupUI
+@end
--- Sharing/Sharing/ViewController.swift
+++ Sharing/Sharing/ViewController.swift
@@ -8,36 +8,47 @@
 
 import UIKit
 import ReplayKit
+import Socket
 
 class ViewController: UIViewController {
+    
+    var socket = try! Socket.create(family: .inet, type: .datagram, proto: .udp)
 
-    var boradcastAVC: RPBroadcastActivityViewController?
+//    var boradcastAVC: RPBroadcastActivityViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
 
 
     @IBAction func startRecord(_ sender: UIButton) {
+        let names: [String] = ["Alice", "Bob", "Jessy"]
         
-        debugPrint(#function)
-        
-        RPBroadcastActivityViewController.load { [weak self] (broadcastActivityViewController, error) in
-            guard let controller = broadcastActivityViewController else {
-                return
+        for name in names {
+            if let data = name.data(using: .utf8) {
+                try! socket.write(from: data, to: Socket.createAddress(for: "192.168.0.101", on: 9999)!)
             }
-            
-            controller.delegate = self
-            
-            
-//            self?.boradcastAVC = controller
-//            self?.boradcastAVC?.delegate = self
-            
-            self?.present(controller, animated: true, completion: nil)
-            
+
+            var d = Data()
+            try? socket.readDatagram(into: &d)                                                
+            print(String(data: d, encoding: .utf8))
         }
+        
+    
+//        RPBroadcastActivityViewController.load(withPreferredExtension: "io.github.rsenjoyer.Gao.Sharing.UploadSetupUI") { [weak self] (broadcastActivityViewController, error) in
+//            guard let controller = broadcastActivityViewController else {
+//                return
+//            }
+//
+//            controller.delegate = self
+//
+//
+////            self?.boradcastAVC = controller
+////            self?.boradcastAVC?.delegate = self
+//
+//            self?.present(controller, animated: true, completion: nil)
+//
+//        }
 
     }
     
@@ -54,16 +65,16 @@ extension ViewController: RPBroadcastActivityViewControllerDelegate {
         // 回调到才此处，上面有两个ViewConttroller
         // RPBroadcastActivityViewController
         // RPBroadcastActivityHostViewController
-        DispatchQueue.main.sync {
-            debugPrint(broadcastActivityViewController)
-    
-//            broadcastActivityViewController.dismiss(animated: true, completion: nil)            
-            self.boradcastAVC = broadcastActivityViewController
-        }
-        
-        broadcastController?.startBroadcast(handler: { (error) in
-            debugPrint(error)
-        })
+//        DispatchQueue.main.sync {
+//            debugPrint(broadcastActivityViewController)
+//
+//            broadcastActivityViewController.dismiss(animated: true, completion: nil)
+//            self.boradcastAVC = broadcastActivityViewController
+//        }
+//
+//        broadcastController?.startBroadcast(handler: { (error) in
+//            debugPrint(error)
+//        })
     }
     
 
